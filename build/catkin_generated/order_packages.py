282f14f854e8ea492ad4716e7bbbe8087faf43d1
suchetanrs <suchetan.saravanan@gmail.com>
2022-12-07 00:45:42 +0500

    first

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/suchetan/Ingen_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/suchetan/yeti_ws/devel;/home/suchetan/sample/devel;/home/suchetan/snakebot_ws/devel;/home/suchetan/catkin_ws/devel;/home/suchetan/kratos/devel;/home/suchetan/hackathons/devel;/home/suchetan/InGenDynamics/devel;/opt/ros/melodic'.split(';') if '/home/suchetan/yeti_ws/devel;/home/suchetan/sample/devel;/home/suchetan/snakebot_ws/devel;/home/suchetan/catkin_ws/devel;/home/suchetan/kratos/devel;/home/suchetan/hackathons/devel;/home/suchetan/InGenDynamics/devel;/opt/ros/melodic' != '' else []
